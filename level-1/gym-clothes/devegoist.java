d1bddb4dd0928b237a3e1567f49f2a17ca48b4ca
devegoist <devegoist@gmail.com>
2019-02-05 20:16:39 +0900

    level-1/gym-clothes/devegoist

---
+++ level-1/gym-clothes/devegoist.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j] ||
+                    lost[i] - reserve[j] == 1 ||
+                    lost[i] - reserve[j] == -1) {
+
+                    reserve[j] = -1;
+                    break;
+                }
+
+                if (j == reserve.length - 1) {
+                    answer -= 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
