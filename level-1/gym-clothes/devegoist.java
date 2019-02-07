caf706fbb8d5ba1cbab69b66bc24136ea83a14ea
devegoist <devegoist@gmail.com>
2019-02-07 21:13:06 +0900

    코드 수정. lost 가 reserve 인 경우를 미리 계산

--- level-1/gym-clothes/devegoist.java
+++ level-1/gym-clothes/devegoist.java
@@ -1,6 +1,15 @@
 public class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n;
+        
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
 
         for (int i = 0; i < lost.length; i++) {
             for (int j = 0; j < reserve.length; j++) {
