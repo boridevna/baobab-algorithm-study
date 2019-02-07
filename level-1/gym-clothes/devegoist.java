e1057c1519d03e3c4871ae83007b2b7833966f1d
devegoist <devegoist@gmail.com>
2019-02-07 21:31:11 +0900

    수정 수정

--- level-1/gym-clothes/devegoist.java
+++ level-1/gym-clothes/devegoist.java
@@ -1,10 +1,11 @@
 public class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n;
-        
+
         for (int i = 0; i < lost.length; i++) {
             for (int j = 0; j < reserve.length; j++) {
                 if (lost[i] == reserve[j]) {
+                    lost[i] = -1;
                     reserve[j] = -1;
                     break;
                 }
